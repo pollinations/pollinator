76d5f1c31ff2257e8613fae02553b3dd16a651ee
Niels Warncke <niels.warncke@gmail.com>
2022-06-01 20:58:33 +0200

    linting

--- pollinator/process_msg.py
+++ pollinator/process_msg.py
@@ -1,14 +1,16 @@
-import os
-import subprocess
-import time
-import tempfile
 import json
-import shutil
-import requests
 import logging
+import os
+import shutil
+import subprocess
+import tempfile
+import time
+
+import requests
 from retry import retry
 
 from pollinator.constants import images
+
 
 def debug(f):
     def debugged(*args, **kwargs):
@@ -18,15 +20,14 @@ def debug(f):
             logging.error(type(e), e)
             breakpoint()
             f(*args, **kwargs)
+
     return debugged
 
 
 @retry(tries=10, delay=1)
 def send_to_cog_container(message, output_path):
     # Send message to cog container
-    payload = {
-        "inputs": message['inputs']
-    }  
+    payload = {"inputs": message["inputs"]}
     response = requests.post("http://localhost:6421/predictions", json=payload)
 
     logging.info(f"response: {response} {response.text}")
@@ -35,7 +36,9 @@ def send_to_cog_container(message, output_path):
         logging.error(response.text)
         with open(f"{output_path}/error.txt", "w") as f:
             f.write(response.text)
-        raise Exception(f"Error while sending message to cog container: {response.text}")
+        raise Exception(
+            f"Error while sending message to cog container: {response.text}"
+        )
 
     return response
 
@@ -51,13 +54,13 @@ def kill_cog_container(container_id_file):
 
 
 def start_cog_container(message, output_path, container_id_file):
-    image = images[message['notebook']]
-    gpus = "" if True else "--gpus all" # TODO check if GPU is available
+    image = images[message["notebook"]]
+    gpus = "" if True else "--gpus all"  # TODO check if GPU is available
     # Start cog container
     cog_cmd = (
-        f'docker run --rm --detach --cidfile {container_id_file} --publish 6421:5000 '
-        f'--mount type=bind,source={output_path},target=/src/output '
-        f'{gpus} {image}'
+        f"docker run --rm --detach --cidfile {container_id_file} --publish 6421:5000 "
+        f"--mount type=bind,source={output_path},target=/src/output "
+        f"{gpus} {image}"
     )
     logging.info(cog_cmd)
     os.system(cog_cmd)
@@ -79,7 +82,7 @@ def process_message(message):
     container_id_file = "./container_id"
 
     prepare_output_folder(output_path, container_id_file)
-    
+
     # # Start IPFS syncinv=g
     # ipfs_pid = subprocess.Popen(
     #     f"pollinate --send --ipns --nodeid {message['pollen_id']}"
@@ -91,7 +94,7 @@ def process_message(message):
     time.sleep(10)
     send_to_cog_container(message, output_path)
     kill_cog_container(container_id_file)
-    
+
     # # kill pollinate
     # time.sleep(5)
     # subprocess.Popen(["kill", str(ipfs_pid)])
--- pollinator/sqs_consumer.py
+++ pollinator/sqs_consumer.py
@@ -1,22 +1,21 @@
-import os
 import json
 import logging
+import os
 
 import boto3
-from botocore.config import Config
 import botocore
 import click
+from botocore.config import Config
 from retry import retry
 
 from pollinator.process_msg import process_message
 
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
-
-AWS_REGION =  os.environ.get('AWS_REGION', "us-east-1")
+AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
 boto3_config = Config(
-    region_name = AWS_REGION,
+    region_name=AWS_REGION,
 )
 sqs = None
 
@@ -25,18 +24,27 @@ sqs = None
 def wait_for_queue_url(aws_endpoint):
     logging.info(f"Trying to get queue {os.environ['QUEUE_NAME']}")
     global sqs, queue_url
-    sqs = boto3.client('sqs', config=boto3_config, region_name=AWS_REGION,
-                         endpoint_url=aws_endpoint)
-    queue_url = sqs.get_queue_url(
-            QueueName=os.environ["QUEUE_NAME"]
-        )['QueueUrl']
+    sqs = boto3.client(
+        "sqs", config=boto3_config, region_name=AWS_REGION, endpoint_url=aws_endpoint
+    )
+    queue_url = sqs.get_queue_url(QueueName=os.environ["QUEUE_NAME"])["QueueUrl"]
     assert queue_url is not None
     logging.info(f"Got queue url: {queue_url}")
 
 
 @click.command()
-@click.option("--aws_endpoint", type=str, default=None, help="For localstack: http://localhost:4566 | For AWS: None")
-@click.option("--aws_profile", type=str, default=None, help="For localstack: localstack | For AWS: aws_profile")
+@click.option(
+    "--aws_endpoint",
+    type=str,
+    default=None,
+    help="For localstack: http://localhost:4566 | For AWS: None",
+)
+@click.option(
+    "--aws_profile",
+    type=str,
+    default=None,
+    help="For localstack: localstack | For AWS: aws_profile",
+)
 def main(aws_endpoint=None, aws_profile=None):
     """Poll for new messages and process them."""
     logging.info("Starting SQS consumer...")
@@ -46,33 +54,28 @@ def main(aws_endpoint=None, aws_profile=None):
         try:
             response = sqs.receive_message(
                 QueueUrl=queue_url,
-                AttributeNames=[
-                    'SentTimestamp'
-                ],
+                AttributeNames=["SentTimestamp"],
                 MaxNumberOfMessages=1,
-                MessageAttributeNames=[
-                    'All'
-                ],
+                MessageAttributeNames=["All"],
                 VisibilityTimeout=100,
-                WaitTimeSeconds=5
+                WaitTimeSeconds=5,
             )
         except botocore.exceptions.ReadTimeoutError:
             continue
-        if 'Messages' not in response:
+        if "Messages" not in response:
             continue
 
-        messages = response['Messages']
+        messages = response["Messages"]
 
         for message in messages:
             # Delete received message from queue
             try:
-                process_message(json.loads(message['Body']))
+                process_message(json.loads(message["Body"]))
             except Exception as e:
                 logging.error(f"exception while processing message: {str(e)}")
 
             sqs.delete_message(
-                QueueUrl=queue_url,
-                ReceiptHandle=message['ReceiptHandle']
+                QueueUrl=queue_url, ReceiptHandle=message["ReceiptHandle"]
             )
 
 
