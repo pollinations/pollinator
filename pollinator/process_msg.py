238f8473f06cf7cc26437c1f4dd78d61e6a90d41
Niels Warncke <niels.warncke@gmail.com>
2022-06-02 18:27:47 +0200

    refactored process_msg to use context managers

--- pollinator/process_msg.py
+++ pollinator/process_msg.py
@@ -3,11 +3,59 @@ import os
 import shutil
 import time
 import subprocess
+from uuid import uuid4
 
 import requests
 from retry import retry
 
 from pollinator.constants import images
+
+
+class BackgroundCommand:
+    def __init__(self, cmd):
+        self.cmd = cmd
+
+    def __enter__(self):
+        self.proc = subprocess.Popen(
+            self.cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        return self.proc
+
+    def __exit__(self, type, value, traceback):
+        time.sleep(5)
+        self.proc.kill()
+        self.proc.wait()
+        self.proc.stdout.close()
+        self.proc.stderr.close()
+
+
+
+class RunningCogModel:
+    def __init__(self, message, output_path):
+        self.message = message
+        self.output_path = output_path
+        self.container_id_file = str(uuid4().hex)
+
+    def __enter__(self):
+        image = images[self.message["notebook"]]
+        gpus = "--gpus all"  # TODO check if GPU is available
+        # Start cog container
+        cog_cmd = (
+            f"docker run --rm --detach --cidfile {self.container_id_file} --network host "
+            f"--mount type=bind,source={self.output_path},target=/outputs "
+            f"{gpus} {image}"
+        )
+        logging.info(cog_cmd)
+        os.system(cog_cmd)
+
+    def __exit__(self):
+        if os.path.exists(self.container_id_file):
+            container_id = open(self.container_id_file).read()
+            logging.info(f"Killing cog container: {container_id}")
+            os.system(f"docker kill {container_id}")
+            os.remove(self.container_id_file)
+        else:
+            logging.info("No container id file found")
 
 
 def process_message(message):
@@ -19,25 +67,12 @@ def process_message(message):
     prepare_output_folder(output_path, container_id_file)
 
     # # Start IPFS syncinv=g
-    ipfs_pid = subprocess.Popen(
+    with BackgroundCommand(
         f"pollinate --send --ipns --nodeid {message['pollen_id']}"
-        f" --path {output_path} ",
-        shell=True).pid
-
-    # process message
-    start_cog_container(message, output_path, container_id_file)
-    time.sleep(10)
-    try:
-        send_to_cog_container(message, output_path)
-    except Exception as e:
-        kill_cog_container(container_id_file)
-        time.sleep(5)
-        subprocess.Popen(["kill", str(ipfs_pid)])
-        raise e
-    kill_cog_container(container_id_file)
-    # # kill pollinate
-    time.sleep(5)
-    subprocess.Popen(["kill", str(ipfs_pid)])
+        f" --path {output_path}"
+    ):
+        with RunningCogModel(message, output_path):
+            send_to_cog_container(message, output_path)
 
 
 def prepare_output_folder(output_path, container_id_file):
@@ -48,21 +83,7 @@ def prepare_output_folder(output_path, container_id_file):
         os.remove(container_id_file)
 
 
-def start_cog_container(message, output_path, container_id_file):
-    # docker run --rm -ti --publish 6421:5000 --mount type=bind,source=/tmp,target=/src/output  r8.im/pixray/text2image@sha256:f6ca4f09e1cad8c4adca2c86fd1f4c9121f5f2e6c2f00408ab19c4077192fd23 /bin/bash
-    image = images[message["notebook"]]
-    gpus = "--gpus all"  # TODO check if GPU is available
-    # Start cog container
-    cog_cmd = (
-        f"docker run --rm --detach --cidfile {container_id_file} --network host "
-        f"--mount type=bind,source={output_path},target=/outputs "
-        f"{gpus} {image}"
-    )
-    logging.info(cog_cmd)
-    os.system(cog_cmd)
-
-
-@retry(tries=30, delay=1)
+@retry(tries=90, delay=2)
 def send_to_cog_container(message, output_path):
     # Send message to cog container
     payload = {"input": message["inputs"], "output_file_prefix": str(output_path)}
@@ -79,13 +100,3 @@ def send_to_cog_container(message, output_path):
         )
 
     return response
-
-
-def kill_cog_container(container_id_file):
-    if os.path.exists(container_id_file):
-        container_id = open(container_id_file).read()
-        logging.info(f"Killing cog container: {container_id}")
-        os.system(f"docker kill {container_id}")
-        os.remove(container_id_file)
-    else:
-        logging.info("No container id file found")
