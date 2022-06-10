7c1af8ef8746935c287829e1932c8cc3cea85c1d
Niels Warncke <niels.warncke@gmail.com>
2022-06-10 15:05:26 +0200

    updates available models

--- pollinator/constants.py
+++ pollinator/constants.py
@@ -1,5 +1,3 @@
 images = {
-    "clip+vqgan": "r8.im/pixray/text2image@sha256:f6ca4f09e1cad8c4adca2c86fd1f4c9121f5f2e6c2f00408ab19c4077192fd23",
-    "test-image": "cog-sample",
     "latent-diffusion": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m",
 }
