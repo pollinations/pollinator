3acbd5b87f1db4f1899385b61c2ca57cf14a3986
Niels Warncke <niels.warncke@gmail.com>
2022-06-21 12:40:28 +0200

    add external piano transcription model to test the http->ipfs file

--- pollinator/constants.py
+++ pollinator/constants.py
@@ -1,4 +1,5 @@
 images = {
     "latent-diffusion": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m",
     "envisioning": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-envisioning",
+    "piano-transcription": "r8.im/bytedance/piano-transcription",
 }
