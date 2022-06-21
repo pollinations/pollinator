61f8a436484746cb2d9b433fc6695788f53d5352
Niels Warncke <niels.warncke@gmail.com>
2022-06-21 15:25:17 +0200

    fetch available models from model-index in order to test the http response

--- pollinator/constants.py
+++ pollinator/constants.py
@@ -1,5 +1,23 @@
-images = {
-    "latent-diffusion": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m",
-    "envisioning": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-envisioning",
-    "piano-transcription": "r8.im/bytedance/piano-transcription",
-}
+import requests
+
+
+class JsonFileDict(dict):
+    """Acts like a dict, but loads the data from a file on first access or whenever a key is missing."""
+
+    def __init__(self, file_url):
+        self.file_url = file_url
+        response = requests.get(self.file_url)
+        super().__init__(response.json())
+
+    def __getitem__(self, key):
+        if key not in self:
+            response = requests.get(self.file_url)
+            self.update(response.json())
+        if key in self:
+            return self.get(key)
+        return super().__getitem__(key)
+
+
+images = JsonFileDict(
+    "https://raw.githubusercontent.com/pollinations/model-index/main/images.json"
+)
