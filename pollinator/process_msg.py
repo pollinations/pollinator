2ff516200fad5188024549e7ee2db77ccdfc396e
Thomas Haferlach <t.haferlach@gmail.com>
2022-07-14 21:27:22 +0200

    increase sleep after run

--- pollinator/process_msg.py
+++ pollinator/process_msg.py
@@ -28,7 +28,7 @@ class BackgroundCommand:
         return self.proc
 
     def __exit__(self, type, value, traceback):
-        time.sleep(20)
+        time.sleep(15)
         logging.info(f"Killing background command: {self.cmd}")
         self.proc.kill()
         logs, errors = self.proc.communicate()
