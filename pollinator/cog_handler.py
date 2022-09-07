9718c8772e4c00e76fb7903714023821d122b8d0
Niels Warncke <niels.warncke@gmail.com>
2022-09-07 16:23:44 +0200

    Kill cog after 100 invocations (#43)
    * kill cog after 100 invocations
    
    * add docker system prune command again
    
    * linting
    
    * timeout for pollinate-cli.js
    
    * linting
    
    Co-authored-by: Thomas Haferlach <thomash@pollinations.ai>

--- pollinator/cog_handler.py
+++ pollinator/cog_handler.py
@@ -20,7 +20,7 @@ class UnhealthyCogContainer(Exception):
 
 
 loaded_model = None
-# MAX_NUM_POLLEN_UNTIL_RESTART = 30
+MAX_NUM_POLLEN_UNTIL_RESTART = 100
 
 
 class RunningCogModel:
@@ -35,13 +35,15 @@ class RunningCogModel:
     def __enter__(self):
         global loaded_model
         # Check if the container is already running
-        running_images = [
-            container.image for container in docker_client.containers.list()
-        ]
         self.pollen_start_time = dt.datetime.now()
+        try:
+            running_image = docker_client.containers.get("cogmodel").image
+        except docker.errors.NotFound:
+            running_image = None
         if (
-            self.image in running_images
-        ):  # and self.pollen_since_container_start < MAX_NUM_POLLEN_UNTIL_RESTART:
+            self.image == running_image
+            and self.pollen_since_container_start < MAX_NUM_POLLEN_UNTIL_RESTART
+        ):
             self.pollen_since_container_start += 1
             logging.info(f"Model already loaded: {self.image}")
             return self
