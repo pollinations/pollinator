28dbe137dce7cbadde9a98fa87afce38aaf3dcb4
Niels Warncke <niels.warncke@gmail.com>
2022-11-22 11:54:23 +0100

    logging which tasks it fetches and which model it thinks it has loaded

--- pollinator/main.py
+++ pollinator/main.py
@@ -135,6 +135,7 @@ def shutdown_pollinator():
 
 
 def maybe_process(message):
+    logging.info(f"Checking tasks for: {message['image']} - loaded model: {cog_handler.loaded_model}")
     check_pollinator_updates()
     if message["image"] not in constants.available_models():
         logging.info(f"Ignoring message for {message['image']}")
